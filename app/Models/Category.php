3e1dfae1537df1a7fed91e9c41ce4615422a34f2
rzilvinas <zilvinas.ragauskas@gmail.com>
2022-02-09 01:53:39 +0200

    Define categories mutator for slug column

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,13 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model {
+class Category extends Model
+{
 
-   use HasFactory;
+    use HasFactory;
 
-   protected $fillable = ['title','description','slug'];
+    protected $fillable = ['title', 'description', 'slug'];
 
+    /**
+     * Set the category slug.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setSlugAttribute($value)
+    {
+        $slugValue = Str::slug($value); //Value can become empty after trying to convert to slug so we try converting it first
+
+        if(empty($slugValue)) {
+            $slugValue = Str::slug($this->attributes['title']);
+        } 
+
+        $this->attributes['slug'] = $slugValue;
+    }
 }
